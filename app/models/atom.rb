56fa2c8c8d3c269210a1dbcfea44d30c9b5a0ca1
Madhusudan Khadka <sudankhd@gmail.com>
2013-06-28 20:29:52 -0400

    user_ids added

--- app/models/atom.rb
+++ app/models/atom.rb
@@ -1,5 +1,5 @@
 class Atom < ActiveRecord::Base
-  attr_accessible :story, :title, :approved
+  attr_accessible :story, :title, :approved, :user_ids
   has_many :atom_users
   has_many :users, through: :atom_users
 end
